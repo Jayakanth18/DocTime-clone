ae3aed078a8ff71200a09e7a15d4843e2f287f5e
Jayakanth <jayakanth1812@gmail.com>
2024-01-02 12:10:36 +0530

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import { Clients } from "./components/Clients";
 import Footer from "./components/Footer";
 import Stats from "./components/Stats";
 import Form from "./components/Form";
+
 function App() {
   return (
     <>
-      {/* <h1 className="text-2xl flex justify-center">Welcome</h1> */}
       <Navbar />
       <LandingPage />
       <Details />
